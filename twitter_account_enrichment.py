003fc88e28c4f6c8f83846962d9984a9c13267f4
orly2000 <orly2000@gmail.com>
2022-04-30 20:02:41 +0300

    organized data sets + wrote script for enrichment of a single tweeter account id

--- twitter_account_enrichment.py
+++ twitter_account_enrichment.py
@@ -1,4 +1,5 @@
 from typing import List, Dict
+import json
 import requests
 
 bearer_token = "AAAAAAAAAAAAAAAAAAAAAK%2FHbgEAAAAAcNNLTzuXuV6ThP3fz30PTJOx294%3DAGpkjKXSxcxnEzeyPJB7XVbeP20TjmLhzpdpMndBB0gIiYk5Dk"
@@ -7,7 +8,21 @@ bearer_token = "AAAAAAAAAAAAAAAAAAAAAK%2FHbgEAAAAAcNNLTzuXuV6ThP3fz30PTJOx294%3D
 def get_users_lookup_url(account_ids: List[str]) -> str:
     account_ids_str = ','.join(account_ids)
     url = f"https://api.twitter.com/2/users?ids={account_ids_str}"
-    print(url)
+    return url
+
+
+def get_user_tweets_url(account_id: str) -> str:
+    url = f"https://api.twitter.com/2/users/{account_id}/tweets"
+    return url
+
+
+def get_user_followers_url(account_id: str) -> str:
+    url = f"https://api.twitter.com/2/users/{account_id}/followers"
+    return url
+
+
+def get_user_following_url(account_id: str) -> str:
+    url = f"https://api.twitter.com/2/users/{account_id}/following"
     return url
 
 
@@ -26,31 +41,67 @@ def connect_to_endpoint(url: str, params: Dict[str, str]) -> Dict:
         )
     return response.json()
 
-#
-# users_list = ['pankajtiwari2']
-# accounts_ids = ["787405734442958848", "796216118331310080"]
-#
-# users_lookup_params = {
-#     "user.fields": "id,name,username,created_at,description,location,pinned_tweet_id,protected,public_metrics,"
-#                    "verified,withheld"
-# }
-#
-# users_lookup_url = get_users_lookup_url(accounts_ids)
-#
-# json_response = connect_to_endpoint(users_lookup_url, users_lookup_params)
-#
-# print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-# print(json_response)
+
+def enrich_users(accounts_ids: List[str]) -> Dict:
+    users_lookup_params = {
+        "user.fields": "id,name,username,created_at,description,location,pinned_tweet_id,protected,public_metrics,"
+                       "verified,withheld"
+    }
+    users_lookup_url = get_users_lookup_url(accounts_ids)
+
+    json_response = connect_to_endpoint(users_lookup_url, users_lookup_params)
+
+    return json_response['data']
 
 
-# {'data': [
-#     {'username': 'best_in_dumbest',
-#      'public_metrics': {'followers_count': 1953, 'following_count': 5, 'tweet_count': 15723, 'listed_count': 56},
-#      'description': 'Blame @xaiax, Inspired by @MakingInvisible, using cmu phonetic data to produce incongruous matches.\nSome images via Lorem Flickr.',
-#      'protected': False, 'verified': False, 'created_at': '2016-10-15T21:32:11.000Z',
-#      'pinned_tweet_id': '787407020156522496', 'name': 'The Best In Dumbest', 'id': '787405734442958848'},
-#     {'location': 'United States', 'username': 'CJRubinPhoto',
-#      'public_metrics': {'followers_count': 800, 'following_count': 838, 'tweet_count': 251, 'listed_count': 5},
-#      'description': 'Photographing the American West since 1980. I specialize in location portraits & events, both indoors & outside, using natural light & portable studio lighting.',
-#      'protected': False, 'verified': False, 'created_at': '2016-11-09T05:01:30.000Z', 'name': 'CJ Rubin',
-#      'id': '796216118331310080'}]}
+def get_user_tweets(account_id: str) -> Dict:
+    user_tweets_params = {
+        "tweet.fields": "attachments,context_annotations,conversation_id,created_at,entities,id,in_reply_to_user_id,lang,public_metrics,reply_settings,source,text",
+        "max_results": 100
+    }
+    user_tweets_url = get_user_tweets_url(account_id)
+
+    json_response = connect_to_endpoint(user_tweets_url, user_tweets_params)
+
+    return json_response['data']
+
+
+def get_user_followers(account_id: str) -> Dict:
+    user_followers_params = {
+        "user.fields": "created_at,description,entities,id,location,name,pinned_tweet_id,public_metrics,username",
+        "max_results": 1000
+    }
+    user_tweets_url = get_user_followers_url(account_id)
+
+    json_response = connect_to_endpoint(user_tweets_url, user_followers_params)
+
+    return json_response['data']
+
+
+def get_user_following(account_id: str) -> Dict:
+    user_followers_params = {
+        "user.fields": "created_at,description,entities,id,location,name,pinned_tweet_id,public_metrics,username",
+        "max_results": 1000
+    }
+    user_following_url = get_user_following_url(account_id)
+
+    json_response = connect_to_endpoint(user_following_url, user_followers_params)
+
+    return json_response['data']
+
+
+user_account_id = "953692489"
+enriched_user = enrich_users([user_account_id])
+user_tweets = get_user_tweets(user_account_id)
+user_followers = get_user_followers(user_account_id)
+user_following = get_user_following(user_account_id)
+
+res = {
+    "user_details": enriched_user,
+    "user_tweets": user_tweets,
+    "user_followers": user_followers,
+    "user_following": user_following
+}
+
+with open(f"res-{user_account_id}.json", "w") as fh:
+    json.dump(res, fh)
