ce3cd02a91c471c820986a11c12fccd95c639e99
orly2000 <orly2000@gmail.com>
2022-05-22 13:45:42 +0300

    edited main notebook - introduction, background etc.

--- utils.py
+++ utils.py
@@ -5,6 +5,9 @@ from typing import List
 def merge_dfs_files(files_paths: List[str]) -> pd.DataFrame:
     original_dfs = []
     for file_path in files_paths:
-        original_dfs.append(pd.read_csv(file_path))
-    merged_df = pd.concat(original_dfs)
+        current_df = pd.read_csv(file_path)
+        current_df = current_df[current_df['account_id'].notna()]
+        current_df = current_df.astype({"account_id": "int64"})
+        original_dfs.append(current_df)
+    merged_df = pd.concat(original_dfs, ignore_index=True)
     return merged_df
